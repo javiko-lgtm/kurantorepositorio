790fe3111a158464cda5b317b9edad45a52d342d
javiko-lgtm <javiko@gmail.com>
2025-08-13 16:32:16 -0400

    Crear archivo src/assets.js con la definición de assets

---
+++ src/assets.js
@@ -0,0 +1,11 @@
+export const assets = {
+  chapita: null, // Si tienes chapita.png pon aquí la imagen
+  payaso: null,  // Si tienes payaso.png pon aquí la imagen
+  completo: null,// Si tienes completo.png pon aquí la imagen
+  fondo: null    // Si tienes fondo1.png pon aquí la imagen
+};
+// Si quieres usar imágenes, descomenta y coloca en assets/:
+// assets.chapita = new Image(); assets.chapita.src = "assets/chapita.png";
+// assets.payaso = new Image(); assets.payaso.src = "assets/payaso.png";
+// assets.completo = new Image(); assets.completo.src = "assets/completo.png";
+// assets.fondo = new Image(); assets.fondo.src = "assets/fondo1.png";
